40dba32a0b230110acb880cff17bd1d93163a868
Luciano Lima <beavess@gmail.com>
2020-03-02 02:04:09 -0300

    Change test renderer to @testing-library/react-native
    Run a test using @testing-library render.

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -4,10 +4,17 @@
 
 import 'react-native';
 import React from 'react';
+import { render, toJSON } from '@testing-library/react-native';
+
 import App from '../App';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+describe('<App />', () => {
+  it('should render properly', () => {
+    const { baseElement } = render(<App />);
+    expect(toJSON(baseElement)).toMatchSnapshot();
+  });
+});
+
 
 it('renders correctly', () => {
   renderer.create(<App />);

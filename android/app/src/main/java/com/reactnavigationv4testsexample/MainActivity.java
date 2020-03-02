66511190b6324e336523321243adc1b915447898
Luciano Lima <beavess@gmail.com>
2020-03-02 03:23:04 -0300

    Add missing configuration for some dependencies

--- android/app/src/main/java/com/reactnavigationv4testsexample/MainActivity.java
+++ android/app/src/main/java/com/reactnavigationv4testsexample/MainActivity.java
@@ -1,6 +1,9 @@
 package com.reactnavigationv4testsexample;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +14,15 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "ReactNavigationV4TestsExample";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@
  * @format
  */
 
+import 'react-native-gesture-handler';
 import React from 'react';
 import {
   SafeAreaView,

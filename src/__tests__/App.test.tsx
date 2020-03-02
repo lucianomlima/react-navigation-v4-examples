b932772e110948f812d1708aa4c947bf27961994
Luciano Lima <beavess@gmail.com>
2020-03-02 02:08:55 -0300

    Add StackNavigator tests

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -4,6 +4,9 @@
 
 import 'react-native';
 import React from 'react';
+import { View, Text } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import { render, toJSON } from '@testing-library/react-native';
 
 import App from '../App';
@@ -15,6 +18,24 @@ describe('<App />', () => {
   });
 });
 
+const TestScreen = () => {
+  return (
+    <View>
+      <Text>Test Screen</Text>
+    </View>
+  );
+};
+
+describe('StackNavigator', () => {
+  it('should render properly', () => {
+    const Navigator = createStackNavigator({
+      Home: TestScreen,
+    });
+    const Container = createAppContainer(Navigator);
+    const { baseElement } = render(<Container />);
+    expect(toJSON(baseElement)).toMatchSnapshot();
+  });
+});
 
 it('renders correctly', () => {
   renderer.create(<App />);
